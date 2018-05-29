e4f3ba92b73949d5c0b896cc84286dd0dfd0ecec
Adriano Ferreira <adriano.f@onthegosystems.com>
2018-05-29 08:37:31 -0300

    fixup

--- src/st/strategy/custom-field/parser/WPML_PB_Custom_Field_Parser_Factory.php
+++ src/st/strategy/custom-field/parser/WPML_PB_Custom_Field_Parser_Factory.php
@@ -4,16 +4,8 @@ class WPML_PB_Custom_Field_Parser_Factory {
 
 	private $config;
 
-	public function __construct( WPML_PB_Custom_Field_Config $config ) {
-		$this->config = $config;
-	}
-
 	public function create_parser() {
-		$parser = null;
-
-		if ( 'json' === $this->config->get_format() ) {
-			$parser = new WPML_PB_Custom_Field_Parser( new WPML_PB_Custom_Field_JSON_Parser() );
-		}
+		new WPML_PB_Custom_Field_Parser( new WPML_PB_Custom_Field_JSON_Parser() );
 
 		return $parser;
 	}
