a35f11179ef5856a7b85b9482e92795e1d69493b
Pierre SYLVESTRE <pierre.s@onthegosystems.com>
2018-09-25 16:56:54 -0300

    Add strings to the content parsed for media usage
    https://onthegosystems.myjetbrains.com/youtrack/issue/wpmlmedia-550

--- src/media/class-wpml-page-builders-media-hooks.php
+++ src/media/class-wpml-page-builders-media-hooks.php
@@ -20,6 +20,7 @@ class WPML_Page_Builders_Media_Hooks implements IWPML_Action {
 	}
 	public function add_hooks() {
 		add_filter( 'wmpl_pb_get_media_updaters', array( $this, 'add_media_updater' ) );
+		add_filter( 'wpml_media_content_for_media_usage', array( $this, 'add_package_strings_content' ), 10, 2 );
 	}
 	/**
 	 * @param IWPML_PB_Media_Update[] $updaters
@@ -31,5 +32,26 @@ class WPML_Page_Builders_Media_Hooks implements IWPML_Action {
 			$updaters[ $this->page_builder_slug ] = $this->media_update_factory->create();
 		}
 		return $updaters;
+	}
+
+	/**
+	 * @param string  $content
+	 * @param WP_Post $post
+	 *
+	 * @return string
+	 */
+	public function add_package_strings_content( $content, $post ) {
+		$packages = apply_filters( 'wpml_st_get_post_string_packages', array(), $post->ID );
+
+		/** @var WPML_Package[] $packages */
+		foreach ( $packages as $package ) {
+			$strings = $package->get_package_strings();
+
+			foreach ( $strings as $string ) {
+				$content .= PHP_EOL . $string->value;
+			}
+		}
+
+		return $content;
 	}
 }
--- tests/phpunit/tests/media/test-wpml-page-builders-media-hooks.php
+++ tests/phpunit/tests/media/test-wpml-page-builders-media-hooks.php
@@ -16,10 +16,12 @@ class Test_WPML_Page_Builders_Media_Hooks extends \OTGS\PHPUnit\Tools\TestCase {
 
 	/**
 	 * @test
+	 * @group wpmlmedia-550
 	 */
 	public function it_should_add_hooks() {
 		$subject = $this->get_subject();
 		\WP_Mock::expectFilterAdded( 'wmpl_pb_get_media_updaters', array( $subject, 'add_media_updater' ) );
+		\WP_Mock::expectFilterAdded( 'wpml_media_content_for_media_usage', array( $subject, 'add_package_strings_content' ), 10, 2 );
 		$subject->add_hooks();
 	}
 
@@ -50,6 +52,63 @@ class Test_WPML_Page_Builders_Media_Hooks extends \OTGS\PHPUnit\Tools\TestCase {
 		$this->assertInstanceOf( 'IWPML_PB_Media_Update', $filtered_updaters[ self::SLUG_TEST ] );
 	}
 
+	/**
+	 * @test
+	 * @group wpmlmedia-550
+	 */
+	public function it_should_not_alter_content_if_no_package() {
+		$content = 'Some content';
+		$post    = $this->get_post();
+
+		\WP_Mock::onFilter( 'wpml_st_get_post_string_packages' )
+			->with( array(), $post->ID )->reply( array() );
+
+		$subject = $this->get_subject();
+
+		$this->assertSame( $content, $subject->add_package_strings_content( $content, $post ) );
+	}
+
+	/**
+	 * @test
+	 * @group wpmlmedia-550
+	 */
+	public function it_should_not_alter_content_if_no_strings_in_package() {
+		$content = 'Some content';
+		$post    = $this->get_post();
+
+		\WP_Mock::onFilter( 'wpml_st_get_post_string_packages' )
+			->with( array(), $post->ID )->reply( array( $this->get_strings_package() ) );
+
+		$subject = $this->get_subject();
+
+		$this->assertSame( $content, $subject->add_package_strings_content( $content, $post ) );
+	}
+
+	/**
+	 * @test
+	 * @group wpmlmedia-550
+	 */
+	public function it_should_add_packages_strings_to_the_content() {
+		$content = 'Some content';
+		$post    = $this->get_post();
+
+		$string1 = (object) array( 'value' => 'String 1' );
+		$string2 = (object) array( 'value' => 'String 2' );
+		
+		$package1 = $this->get_strings_package( array( $string1 ) );
+		$package2 = $this->get_strings_package( array( $string2 ) );
+
+		\WP_Mock::onFilter( 'wpml_st_get_post_string_packages' )
+			->with( array(), $post->ID )->reply( array( $package1, $package2 ) );
+
+		$subject = $this->get_subject();
+
+		$this->assertSame(
+			$content . PHP_EOL . $string1->value . PHP_EOL . $string2->value,
+			$subject->add_package_strings_content( $content, $post )
+		);
+	}
+
 	private function get_subject( $media_update_factory = null ) {
 		$media_update_factory = $media_update_factory ? $media_update_factory : $this->get_media_update_factory();
 		return new WPML_Page_Builders_Media_Hooks( $media_update_factory, self::SLUG_TEST );
@@ -62,6 +121,20 @@ class Test_WPML_Page_Builders_Media_Hooks extends \OTGS\PHPUnit\Tools\TestCase {
 
 	private function get_media_update() {
 		return $this->getMockBuilder( 'IWPML_PB_Media_Update' )->getMock();
+	}
+
+	private function get_post() {
+		$post     = $this->getMockBuilder( 'WP_Post' )->getMock();
+		$post->ID = mt_rand( 1, 10 );
+		return $post;
+	}
+
+	private function get_strings_package( $strings = array() ) {
+		$package = $this->getMockBuilder( 'WPML_Package' )
+		            ->setMethods( array( 'get_package_strings' ) )
+		            ->getMock();
+		$package->method( 'get_package_strings' )->willReturn( $strings );
+		return $package;
 	}
 }
 
